2efabcf5a861145c5926c73ab1c6fa4f14343f25
Clara Mae Wells <clara.wells@selu.edu>
2017-09-19 17:12:25 -0700

    Added image, key points, and brief introduction

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
 'use strict';
 
-//Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not.
-//It's up to you! It's your project!
-
-//The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no
-//responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering
-//all-caps Y/N or YES/NO answers, too.
-
-//Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console
-// for each question of the guessing game.
-
-//Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
-
-//Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good
-//commit messages that explain the WHY of the commit.
 
 var userName = prompt('Thanks for visiting my About Me page! What\'s your name?');
-
 alert('Hi, ' + userName + ', nice to meet you! Let\'s see if you can guess the correct answers to the following five questions about me.');
 
 // Question 1
@@ -36,8 +21,10 @@ var soggyBread = prompt('Do I love eating soggy bread?').toLowerCase();
 console.log('Soggy bread answer is ' + soggyBread);
 
 if (soggyBread === 'n' || soggyBread === 'no') {
+  // User is correct
   alert('And you know this! No gravy on my biscuits, my pizza can\'t set in grease, no sauce dripping on my burger bun.');
 } else {
+  // User is incorrect
   alert('-_- You cannot be serious.');
 }
 
@@ -46,8 +33,10 @@ var eating = prompt('Am I eating right now?').toLowerCase();
 console.log('Currently eating answer is ' + eating);
 
 if (eating === 'n' || eating === 'no') {
+  // User is incorrect
   alert('Silly rabbit. I\'m always eating!');
 } else {
+  // User is correct
   alert('Darn skippy! Want some?');
 }
 
@@ -56,8 +45,10 @@ var lift = prompt('Did I hit the gym this morning?').toLowerCase();
 console.log('Hit the gym answer is ' + lift);
 
 if (lift === 'n' || lift === 'no') {
+  // User is incorrect
   alert('Nah - and if that\'s ever true, please make sure I\'m okay.');
 } else {
+  // User is correct
   alert('Mhmmm...Always. Be. Lifting.');
 }
 
@@ -66,7 +57,9 @@ var codeFellows = prompt('Am I glad I came to Code Fellows?').toLowerCase();
 console.log('Code Fellows answer is ' + codeFellows);
 
 if (codeFellows === 'n' || codeFellows === 'no')  {
+  // User is incorrect
   alert('Aw...I hope you\'re just testing my code. I\'m very glad I came.');
 } else {
+  // User is correct
   alert('Yep. And to think I nearly didn\'t...');
 }
